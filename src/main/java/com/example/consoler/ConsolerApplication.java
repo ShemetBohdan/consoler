d60ab96fe8472ab2bf823cdd247e60fb75fc1777
Bohdan <shemet.bohdan01@gmail.com>
2022-07-06 13:31:30 +0300

    Initial commit

---
+++ src/main/java/com/example/consoler/ConsolerApplication.java
@@ -0,0 +1,13 @@
+package com.example.consoler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsolerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsolerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/consoler/ConsolerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consoler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsolerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
